4b26143b2a8351adee874ce7b954e0f59228a5d9
Unos <129419169+shoc71@users.noreply.github.com>
2026-02-08 00:21:21 -0500

    css-updates & more

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const description = document.getElementsByClassName("displayDescription");
 
-// const maxLength = 50;
-const maxWords = 15;
+const maxLength = 50;
+const maxWords = 10;
 
 for (let des of description) {
     const words = des.textContent.split(" ")
+    const characters = des.textContent.split("")
 
-    if (words.length > maxWords) {
-        des.textContent = words.splice(0, maxWords).join(" ") + " ...";
+    const wordsLimit = words.splice(0, maxWords).join(" ");
+    const charactersLimit = characters.join("").substring(0, maxLength)
+
+    if (wordsLimit.length > maxLength) {
+        des.textContent = charactersLimit + "...";
+    } else if (wordsLimit.length > maxLength) {
+        des.textContent = wordsLimit + "...";
     }
 }
